b61ff7c7475a18d04bdc97116bbb58fb75f9e3cb
Tomáš Kůrka <tom589927@gmail.com>
2021-10-14 19:13:10 +0200

    initial commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import LandingPage from "../pages/landing/LandingPage"
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+      Something is coming
+    </div>
+  )
+}
+
+export default App
