a795959dee5188dfbf69fe42e2ccabb7e6863c27
Tom484 <82095105+Tom484@users.noreply.github.com>
2021-12-20 09:52:08 +0100

    fixed bug with loading

--- client/src/redux/collections/collectionsReducer.js
+++ client/src/redux/collections/collectionsReducer.js
@@ -12,15 +12,26 @@ const collectionsReducer = (state = INITIAL_STATE, action) => {
     case CollectionActions.SAVE_COLLECTION_START:
     case CollectionActions.DELETE_COLLECTION_START:
     case CollectionActions.CREATE_COLLECTION_START:
-    case CollectionActions.FETCH_COLLECTIONS_START:
       return {
         ...state,
         errorMessage: undefined,
         isLoading: true,
       }
+    case CollectionActions.FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        errorMessage: undefined,
+        isFetching: true,
+      }
     case CollectionActions.SAVE_COLLECTION_SUCCESS:
     case CollectionActions.DELETE_COLLECTION_SUCCESS:
     case CollectionActions.CREATE_COLLECTION_SUCCESS:
+      return {
+        ...state,
+        userCollections: action.payload,
+        isLoading: false,
+        errorMessage: undefined,
+      }
     case CollectionActions.FETCH_COLLECTIONS_SUCCESS:
       return {
         ...state,
@@ -31,6 +42,11 @@ const collectionsReducer = (state = INITIAL_STATE, action) => {
     case CollectionActions.SAVE_COLLECTION_FAILURE:
     case CollectionActions.DELETE_COLLECTION_FAILURE:
     case CollectionActions.CREATE_COLLECTION_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload,
+        isLoading: false,
+      }
     case CollectionActions.FETCH_COLLECTIONS_FAILURE:
       return {
         ...state,
