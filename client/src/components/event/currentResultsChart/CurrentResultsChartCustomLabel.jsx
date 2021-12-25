45237f942db7ff79f6e66cace2075bf1a0941dcf
Tomas Kurka <tom589927@gmail.com>
2021-12-25 18:53:16 +0100

    Imrove design overallResults component

--- client/src/components/event/currentResultsChart/CurrentResultsChartCustomLabel.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const CurrentResultsChartCustomLabel = () => {
-  return <div>Ahoj</div>
-}
-
-export default CurrentResultsChartCustomLabel
--- client/src/components/event/overallResults/OverallResults.jsx
+++ client/src/components/event/overallResults/OverallResults.jsx
@@ -1,53 +1,51 @@
 import React from "react"
 import { connect } from "react-redux"
 import { selectEventPlayersArraySorted } from "../../../redux/eventPlayers/eventPlayersSelectors"
-import BubbleBackground from "../../components/bubbleBackground/BubbleBackground"
-import LineBackground from "../../components/lineBackground/LineBackground"
+import CustomBackground from "../../components/customBackground/CustomBackground"
+import { CustomTextExtraLarge } from "../../components/customText/CustomText"
 
 import "./overallResults.scss"
 
 const OverallResults = ({ eventPlayers }) => {
   return (
-    <div className="overall-results">
-      <BubbleBackground />
-      <LineBackground />
-      <div className="overall-results-container">
-        <div className="results">
-          <div className="results-container">
-            <div className="podium">
-              <div className="second podium-spot">
-                <div className="name">{eventPlayers[1]?.displayName}</div>
-                <div className="position second">2</div>
-                <div className="points">{eventPlayers[1]?.score || "0"}</div>
-              </div>
-              <div className="first podium-spot">
-                <div className="name">{eventPlayers[0]?.displayName || "0"}</div>
-                <div className="position first">1</div>
-                <div className="points">{eventPlayers[0]?.score || "0"}</div>
-              </div>
-              <div className="third podium-spot">
-                <div className="name">{eventPlayers[2]?.displayName}</div>
-                <div className="position third">3</div>
-                <div className="points">{eventPlayers[2]?.score || "0"}</div>
-              </div>
-            </div>
-            <div className="other-results">
-              {eventPlayers.length >= 3 &&
-                eventPlayers.map((player, i) => {
-                  if (i < 3) return <></>
-                  return (
-                    <div className="result-box">
-                      <div className="order">{i + 1}.</div>
-                      <div className="name">{player.displayName}</div>
-                      <div className="points">{player.score}</div>
-                    </div>
-                  )
-                })}
-            </div>
+    <CustomBackground className="overall-results">
+      <div className="results-container">
+        <CustomTextExtraLarge center style={{ marginBottom: "20px" }}>
+          Results
+        </CustomTextExtraLarge>
+        <div className="podium">
+          <div className="second podium-spot">
+            <div className="name">{eventPlayers[1]?.displayName}</div>
+            <div className="position second">2</div>
+            <div className="points">{eventPlayers[1]?.score || "0"}</div>
+          </div>
+          <div className="first podium-spot">
+            <div className="name">{eventPlayers[0]?.displayName || "0"}</div>
+            <div className="position first">1</div>
+            <div className="points">{eventPlayers[0]?.score || "0"}</div>
+          </div>
+          <div className="third podium-spot">
+            <div className="name">{eventPlayers[2]?.displayName}</div>
+            <div className="position third">3</div>
+            <div className="points">{eventPlayers[2]?.score || "0"}</div>
           </div>
         </div>
+
+        <div className="other-results">
+          {eventPlayers.length >= 3 &&
+            eventPlayers.map((player, i) => {
+              if (i < 3) return <></>
+              return (
+                <div className="result-box">
+                  <div className="order">{i + 1}.</div>
+                  <div className="name">{player.displayName}</div>
+                  <div className="points">{player.score}</div>
+                </div>
+              )
+            })}
+        </div>
       </div>
-    </div>
+    </CustomBackground>
   )
 }
 
